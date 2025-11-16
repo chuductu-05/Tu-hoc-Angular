55b118b4bd725bf91def12b7d38b465c80dd528b
chuductu-05 <ductu205@gmail.com>
2025-11-16 09:20:13 +0700

    CRUD

--- src/app/component/product/list-product/list-product.ts
+++ src/app/component/product/list-product/list-product.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import {TableModule} form 'prime/'
 @Component({
   selector: 'app-list-product',
-  imports: [],
+  imports: [TableModule],
+
   templateUrl: './list-product.html',
   styleUrl: './list-product.scss',
 })
